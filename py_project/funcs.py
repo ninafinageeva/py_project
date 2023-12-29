10d1691ff33f721fae2856913ce1dfd7d6e1eeda
Nina Finageeva <finageeva.nina@mail.ru>
2023-12-29 14:41:32 +0300

    create and add func subtraction

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -1,3 +1,18 @@
-from funcs import summ, substraction
-print(summ(a:2, b:3))
-print(substraction(a:5, b:3))
+def get_summ(a=2, b=3):
+    """
+    :param a:
+    :param b:
+    :return:
+    int: Результат сложения a и b
+    """
+    return a + b
+
+def subtraction(a, b):
+    """
+    Функция для вычитания
+    :param a (int):
+    :param b (int):
+    :return:
+    int: Результат вычитания b из a
+    """
+    return a - b
