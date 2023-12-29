95144474185f6d053bc0e4323fe756cf96b20223
Nina Finageeva <finageeva.nina@mail.ru>
2023-12-29 14:45:31 +0300

    import func summ from funcs.py

--- py_project/main.py
+++ py_project/main.py
@@ -2,13 +2,3 @@ from funcs import summ, subtraction
 print(summ(a: 2, b: 3))
 print(subtraction(a: 5, b: 3))
 
-
-def subtraction(a, b):
-    """
-    Функция для вычитания
-    :param a (int):
-    :param b (int):
-    :return:
-    int: Результат вычитания b из a
-    """
-    return a - b
